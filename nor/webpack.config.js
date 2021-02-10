7e793c34b704e938dfd53d52fb231a5ca12f8196
JoshuaWoods <nordev@protonmail.com>
2021-02-10 11:53:03 -0600

    webpack

---
+++ nor/webpack.config.js
@@ -0,0 +1,28 @@
+// Add this to a webpack.config.js file
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    site: ['./source/javascripts/site.js'],
+    style: ['./source/stylesheets/site.css.scss'],
+  },
+  output: {
+    path: path.resolve(__dirname, '.tmp/dist'),
+    filename: '[name].min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
